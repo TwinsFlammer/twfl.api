c9abe45b7b066aa32478c060502c02364287cb04
Gutyerrez <srgutyerrez@gmail.com>
2019-12-14 02:50:53 -0300

    Create InventoryBuilder.java

---
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/inventory/InventoryBuilder.java
@@ -0,0 +1,238 @@
+package com.redecommunity.api.spigot.inventory;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.redecommunity.api.spigot.inventory.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.server.v1_8_R3.*;
+import org.apache.commons.lang3.Validate;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftHumanEntity;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftInventory;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.InventoryHolder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class InventoryBuilder extends CraftInventory {
+    private Boolean cancelled = false;
+    private HashMap<Integer, Item> items = Maps.newHashMap();
+
+    private Consumer<InventoryCloseEvent> closeEventConsumer;
+
+    public InventoryBuilder(String name, Integer rows) {
+        super(
+                new MinecraftInventory(
+                        rows * 9,
+                        name
+                )
+        );
+    }
+
+    public InventoryBuilder(String name, InventoryType inventoryType) {
+        super(
+                new MinecraftInventory(
+                        inventoryType,
+                        name
+                )
+        );
+    }
+
+    public void setItem(int index, Item item) {
+        super.setItem(index, item.build());
+
+        this.items.put(index, item);
+    }
+
+    public void addItem(Item item) {
+        for (int i = 0; i < this.getSize(); i++)
+            if (this.getContents()[i] == null || this.getContents()[i].getType() == Material.AIR) {
+                this.setItem(i, item);
+                break;
+            }
+    }
+
+    public void onClick(InventoryClickEvent event) {
+        if (event.getClick() == ClickType.NUMBER_KEY) event.setCancelled(true);
+        event.setCancelled(this.cancelled);
+
+        Integer slot = event.getSlot();
+
+        Item item = this.items.getOrDefault(slot, null);
+
+        if (item != null && item.getInventoryClickEventConsumer() != null)
+            item.getInventoryClickEventConsumer().accept(event);
+    }
+
+    static class MinecraftInventory implements IInventory {
+
+        private final net.minecraft.server.v1_8_R3.ItemStack[] items;
+
+        @Setter
+        @Getter
+        private int maxStackSize = 64;
+
+        @Getter
+        private final List<HumanEntity> viewers;
+
+        @Getter
+        private final String name;
+
+        @Getter
+        private final InventoryType type;
+
+        public MinecraftInventory(int size, String title) {
+            Validate.notNull(title, "Title cannot be null");
+
+            this.items = new net.minecraft.server.v1_8_R3.ItemStack[size];
+            this.name = title;
+            this.viewers = Lists.newArrayList();
+            this.type = InventoryType.CHEST;
+        }
+
+        public MinecraftInventory(InventoryType type, String title) {
+            Validate.notNull(title, "Title cannot be null");
+
+            this.items = new net.minecraft.server.v1_8_R3.ItemStack[type.getDefaultSize()];
+            this.name = title;
+            this.viewers = Lists.newArrayList();
+            this.type = type;
+        }
+
+        @Override
+        public int getSize() {
+            return items.length;
+        }
+
+        @Override
+        public net.minecraft.server.v1_8_R3.ItemStack getItem(int i) {
+            return items[i];
+        }
+
+        @Override
+        public net.minecraft.server.v1_8_R3.ItemStack splitStack(int i, int j) {
+            net.minecraft.server.v1_8_R3.ItemStack stack = this.getItem(i);
+            net.minecraft.server.v1_8_R3.ItemStack result;
+            if (stack == null) {
+                return null;
+            }
+            if (stack.count <= j) {
+                this.setItem(i, null);
+                result = stack;
+            } else {
+                result = CraftItemStack.copyNMSStack(stack, j);
+                stack.count -= j;
+            }
+            this.update();
+            return result;
+        }
+
+        @Override
+        public net.minecraft.server.v1_8_R3.ItemStack splitWithoutUpdate(int i) {
+            net.minecraft.server.v1_8_R3.ItemStack stack = this.getItem(i);
+            net.minecraft.server.v1_8_R3.ItemStack result;
+            if (stack == null) {
+                return null;
+            }
+            if (stack.count <= 1) {
+                this.setItem(i, null);
+                result = stack;
+            } else {
+                result = CraftItemStack.copyNMSStack(stack, 1);
+                stack.count -= 1;
+            }
+            return result;
+        }
+
+        @Override
+        public void setItem(int i, net.minecraft.server.v1_8_R3.ItemStack itemstack) {
+            items[i] = itemstack;
+            if (itemstack != null && this.getMaxStackSize() > 0 && itemstack.count > this.getMaxStackSize()) {
+                itemstack.count = this.getMaxStackSize();
+            }
+        }
+
+        @Override
+        public void update() {
+        }
+
+        @Override
+        public boolean a(EntityHuman entityhuman) {
+            return true;
+        }
+
+        @Override
+        public net.minecraft.server.v1_8_R3.ItemStack[] getContents() {
+            return items;
+        }
+
+        @Override
+        public void onOpen(CraftHumanEntity who) {
+            viewers.add(who);
+        }
+
+        public void onClose(CraftHumanEntity who) {
+            viewers.remove(who);
+        }
+
+        @Override
+        public InventoryHolder getOwner() {
+            return null;
+        }
+
+        public boolean b(int i, net.minecraft.server.v1_8_R3.ItemStack itemstack) {
+            return true;
+        }
+
+        @Override
+        public void startOpen(EntityHuman entityHuman) {
+
+        }
+
+        @Override
+        public void closeContainer(EntityHuman entityHuman) {
+
+        }
+
+        @Override
+        public int getProperty(int i) {
+            return 0;
+        }
+
+        @Override
+        public void b(int i, int i1) {
+
+        }
+
+        @Override
+        public int g() {
+            return 0;
+        }
+
+        @Override
+        public void l() {
+
+        }
+
+        @Override
+        public boolean hasCustomName() {
+            return name != null;
+        }
+
+        @Override
+        public IChatBaseComponent getScoreboardDisplayName() {
+            return new ChatComponentText(name);
+        }
+    }
+}
