7dca61bf29add771539860a3c5d0922323926116
Gutyerrez <srgutyerrez@gmail.com>
2019-12-13 23:37:54 -0300

    Create CommunityPlugin.java

---
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/CommunityPlugin.java
@@ -0,0 +1,33 @@
+package com.redecommunity.api.spigot;
+
+import com.redecommunity.common.shared.Common;
+import com.redecommunity.common.shared.updater.data.Updater;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+/**
+ * Created by @SrGutyerrez
+ */
+@RequiredArgsConstructor
+public abstract class CommunityPlugin extends JavaPlugin {
+    @Override
+    public void onLoad() {
+    }
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+        Updater updater = new Updater(this.getFile(), Common.getBranch());
+
+        try {
+            updater.download();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
