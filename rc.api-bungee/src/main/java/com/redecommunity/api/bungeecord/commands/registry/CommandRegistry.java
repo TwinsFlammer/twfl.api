21cc2e03de24d40c728d0d9822ca4741b7b6d44d
Gutyerrez <srgutyerrez@gmail.com>
2019-12-18 09:46:05 -0300

    Update CommandRegistry.java

--- rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/commands/registry/CommandRegistry.java
+++ rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/commands/registry/CommandRegistry.java
@@ -1,4 +1,4 @@
-package com.redecommunity.api.bungeecord.commands.manager;
+package com.redecommunity.api.bungeecord.commands.registry;
 
 import com.redecommunity.api.bungeecord.CommunityPlugin;
 import com.redecommunity.api.bungeecord.commands.CustomCommand;
@@ -7,8 +7,10 @@ import net.md_5.bungee.api.ProxyServer;
 /**
  * Created by @SrGutyerrez
  */
-public class CommandManager {
+public class CommandRegistry {
     public static void registerCommand(CommunityPlugin communityPlugin, CustomCommand customCommand) {
+        System.out.println("[CommandRegistry] Registering " + customCommand.getName() + " command by " + communityPlugin.getFile().getName() + ".");
+
         ProxyServer.getInstance().getPluginManager().registerCommand(communityPlugin, customCommand);
     }
 }
