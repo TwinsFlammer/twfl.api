5640a329925a663318c649e46fd805f5b62baa43
Gutyerrez <srgutyerrez@gmail.com>
2019-12-18 11:45:00 -0300

    Delete Message.java

--- rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/message/Message.java
+++
@@ -1,8 +0,0 @@
-package com.redecommunity.api.bungeecord.message;
-
-/**
- * Created by @SrGutyerrez
- */
-public class Message {
-    public static final String CHANNEL_NAME = "message_channel";
-}
