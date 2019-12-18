e3f876200f8871b82309a5fbdf517a9dfd92e4ac
Gutyerrez <srgutyerrez@gmail.com>
2019-12-18 11:44:37 -0300

    Update MessageChannel.java

--- rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/message/channel/MessageChannel.java
+++ rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/message/channel/MessageChannel.java
@@ -1,7 +1,7 @@
 package com.redecommunity.api.bungeecord.message.channel;
 
-import com.redecommunity.api.bungeecord.message.Message;
 import com.redecommunity.common.shared.databases.redis.channel.data.Channel;
+import com.redecommunity.common.shared.util.Constants;
 
 /**
  * Created by @SrGutyerrez
@@ -9,6 +9,6 @@ import com.redecommunity.common.shared.databases.redis.channel.data.Channel;
 public class MessageChannel extends Channel {
     @Override
     public String getName() {
-        return Message.CHANNEL_NAME;
+        return Constants.MESSAGE_CHANNEL;
     }
 }
