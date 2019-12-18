48abccf3fcbdadcd1f04a0b5d03b7135cbfad483
Gutyerrez <srgutyerrez@gmail.com>
2019-12-18 09:44:18 -0300

    Create Message.java

---
+++ rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/message/Message.java
@@ -0,0 +1,8 @@
+package com.redecommunity.api.bungeecord.message;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class Message {
+    public static final String CHANNEL_NAME = "message_channel";
+}
