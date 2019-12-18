c0990e7a1038438d81829518ffff9b92e4cb7f46
Gutyerrez <srgutyerrez@gmail.com>
2019-12-18 09:45:47 -0300

    Update StartManager.java

--- rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/manager/StartManager.java
+++ rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/manager/StartManager.java
@@ -1,9 +1,13 @@
 package com.redecommunity.api.bungeecord.manager;
 
 import com.redecommunity.api.bungeecord.BungeeAPI;
+import com.redecommunity.api.bungeecord.commands.CustomCommand;
 import com.redecommunity.api.bungeecord.commands.defaults.disable.manager.DisabledCommandManager;
-import com.redecommunity.api.bungeecord.commands.manager.CommandManager;
+import com.redecommunity.api.bungeecord.commands.registry.CommandRegistry;
+import com.redecommunity.common.shared.Common;
 import com.redecommunity.common.shared.databases.mysql.dao.Table;
+import com.redecommunity.common.shared.databases.redis.channel.data.Channel;
+import com.redecommunity.common.shared.databases.redis.handler.JedisMessageListener;
 import com.redecommunity.common.shared.util.ClassGetter;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Listener;
@@ -15,10 +19,17 @@ public class StartManager {
     public StartManager() {
         new ListenerManager();
         // COMMAND REGISTER
-        new CommandManager();
-        new DataManager();
+        new CommandRegistry();
 
         new DaoManager();
+
+        new DataManager();
+
+        new CommandManager();
+
+        new ChannelManager();
+
+        new JedisMessageListenerManager();
     }
 }
 
@@ -55,6 +66,57 @@ class DaoManager {
                     Table table = (Table) clazz.newInstance();
 
                     table.createTable();
+                } catch (InstantiationException | IllegalAccessException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+}
+
+class CommandManager {
+    CommandManager() {
+        ClassGetter.getClassesForPackage(BungeeAPI.class).forEach(clazz -> {
+            if (CustomCommand.class.isAssignableFrom(clazz) && !clazz.equals(CustomCommand.class)) {
+                try {
+                    CustomCommand customCommand = (CustomCommand) clazz.newInstance();
+
+                    CommandRegistry.registerCommand(
+                            BungeeAPI.getInstance(),
+                            customCommand
+                    );
+                } catch (InstantiationException | IllegalAccessException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+}
+
+class ChannelManager {
+    ChannelManager() {
+        ClassGetter.getClassesForPackage(BungeeAPI.class).forEach(clazz -> {
+            if (Channel.class.isAssignableFrom(clazz)) {
+                try {
+                    Channel channel = (Channel) clazz.newInstance();
+
+                    Common.getInstance().getChannelManager().register(channel);
+                } catch (InstantiationException | IllegalAccessException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+}
+
+class JedisMessageListenerManager {
+    JedisMessageListenerManager() {
+        ClassGetter.getClassesForPackage(BungeeAPI.class).forEach(clazz -> {
+            if (JedisMessageListener.class.isAssignableFrom(clazz)) {
+                try {
+                    JedisMessageListener jedisMessageListener = (JedisMessageListener) clazz.newInstance();
+
+                    Common.getInstance().getJedisMessageManager().registerListener(jedisMessageListener);
                 } catch (InstantiationException | IllegalAccessException exception) {
                     exception.printStackTrace();
                 }
