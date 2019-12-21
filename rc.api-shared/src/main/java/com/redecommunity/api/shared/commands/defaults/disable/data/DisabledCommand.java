0c5b526be8fb0a356248ec88721fe715fb141b02
Gutyerrez <srgutyerrez@gmail.com>
2019-12-21 09:37:28 -0300

    Update DisabledCommand.java

--- rc.api-shared/src/main/java/com/redecommunity/api/shared/commands/defaults/disable/data/DisabledCommand.java
+++ rc.api-shared/src/main/java/com/redecommunity/api/shared/commands/defaults/disable/data/DisabledCommand.java
@@ -1,4 +1,4 @@
-package com.redecommunity.api.bungeecord.commands.defaults.disable.data;
+package com.redecommunity.api.shared.commands.defaults.disable.data;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
