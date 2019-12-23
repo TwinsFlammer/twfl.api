1df1a3b8c815f5f8ce24d011c9ffcc5c4406c252
Gutyerrez <srgutyerrez@gmail.com>
2019-12-23 12:18:00 -0300

    Create API.java

---
+++ rc.api-shared/src/main/java/com/redecommunity/api/shared/API.java
@@ -0,0 +1,12 @@
+package com.redecommunity.api.shared;
+
+import com.redecommunity.api.shared.manager.StartManager;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class API {
+    public API() {
+        new StartManager();
+    }
+}
