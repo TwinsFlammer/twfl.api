0851b4a372ae173289ebfc1546eccb8ca3591400
Gutyerrez <srgutyerrez@gmail.com>
2020-01-11 07:20:47 -0300

    Create SkinInventory.java

---
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/inventory/SkinInventory.java
@@ -0,0 +1,15 @@
+package com.redecommunity.api.spigot.skin.inventory;
+
+import com.redecommunity.api.spigot.inventory.CustomInventory;
+import com.redecommunity.common.shared.permissions.user.data.User;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class SkinInventory extends CustomInventory {
+    public SkinInventory(User user) {
+        super("Skins", 6);
+
+
+    }
+}
