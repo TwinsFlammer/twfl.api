542bc84db22ccaf8c0fb1b5ecd92d864558f1fc2
Gutyerrez <srgutyerrez@gmail.com>
2020-01-11 07:47:16 -0300

    Update SkinInventory.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/inventory/SkinInventory.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/inventory/SkinInventory.java
@@ -1,7 +1,9 @@
 package com.redecommunity.api.spigot.skin.inventory;
 
 import com.redecommunity.api.spigot.inventory.CustomInventory;
+import com.redecommunity.api.spigot.inventory.item.CustomItem;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import org.bukkit.Material;
 
 /**
  * Created by @SrGutyerrez
@@ -10,6 +12,22 @@ public class SkinInventory extends CustomInventory {
     public SkinInventory(User user) {
         super("Skins", 6);
 
+        user.getSkins().forEach(skin -> {
+            this.addItem(
+                    new CustomItem(Material.SKULL_ITEM)
+                            .name("§e" + skin.getOwner())
+            );
+        });
 
+        this.setItem(
+                49,
+                new CustomItem(Material.BOOK_AND_QUILL)
+                        .name("§cTestando")
+                        .lore(
+                                "SAKDASKDAS",
+                                "SAK@OKSAD"
+                        )
+                        .editable(false)
+        );
     }
 }
