a7fea6db67b4815a185331a50f4668e8e1b3ab50
Gutyerrez <srgutyerrez@gmail.com>
2020-01-11 08:09:43 -0300

    Update SkinCommand.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/command/SkinCommand.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/command/SkinCommand.java
@@ -2,7 +2,11 @@ package com.redecommunity.api.spigot.skin.command;
 
 import com.redecommunity.api.spigot.commands.CustomCommand;
 import com.redecommunity.api.spigot.commands.enums.CommandRestriction;
+import com.redecommunity.api.spigot.skin.inventory.SkinInventory;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 /**
  * Created by @SrGutyerrez
@@ -14,6 +18,10 @@ public class SkinCommand extends CustomCommand {
 
     @Override
     public void onCommand(User user, String[] args) {
+        Inventory inventory = new SkinInventory(user);
 
+        Player player = Bukkit.getPlayer(user.getUniqueId());
+
+        player.openInventory(inventory);
     }
 }
