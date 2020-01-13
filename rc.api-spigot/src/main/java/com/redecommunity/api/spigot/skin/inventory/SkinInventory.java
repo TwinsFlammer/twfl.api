5d407242b2f50e5d158c3253da72cabf8fade971
Gutyerrez <srgutyerrez@gmail.com>
2020-01-13 08:44:49 -0300

    Update SkinInventory.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/inventory/SkinInventory.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/inventory/SkinInventory.java
@@ -2,10 +2,16 @@ package com.redecommunity.api.spigot.skin.inventory;
 
 import com.redecommunity.api.spigot.inventory.CustomInventory;
 import com.redecommunity.api.spigot.inventory.item.CustomItem;
+import com.redecommunity.api.spigot.skin.command.argument.SkinHelpCommand;
+import com.redecommunity.api.spigot.skin.manager.SkinManager;
+import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.skin.data.Skin;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * Created by @SrGutyerrez
@@ -14,19 +20,26 @@ public class SkinInventory extends CustomInventory {
     public SkinInventory(User user) {
         super("Skins", 6);
 
+        Language language = user.getLanguage();
+
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("DD/MM/YYYY");
 
-        user.getSkins().forEach(skin -> {
-            this.addItem(
-                    new CustomItem(Material.SKULL_ITEM)
-                            .data(3)
-                            .name("§e" + skin.getOwner())
-                            .lore("§fUsada pela última vez em: §7" + simpleDateFormat.format(skin.getLastUse()),
-                                    "",
-                                    (skin.isActive() ? "§aSelecionada." : "§eClique para utilizar essa skin."))
-                            .owner(skin.getValue())
-            );
-        });
+        for (int i = 0; i < 50; i++) {
+            user.getSkins()
+                    .stream()
+                    .sorted((o1, o2) -> o2.getLastUse().compareTo(o1.getLastUse()))
+                    .forEach(skin -> {
+                        this.addItem(
+                                new CustomItem(Material.SKULL_ITEM)
+                                        .data(3)
+                                        .name("§e" + skin.getOwner())
+                                        .lore("§fUsada pela última vez em: §7" + simpleDateFormat.format(skin.getLastUse()),
+                                                "",
+                                                (skin.isActive() ? "§aSelecionada." : "§eClique para utilizar essa skin."))
+                                        .owner(skin.getValue())
+                        );
+                    });
+        }
 
         this.setItem(
                 48,
@@ -42,6 +55,15 @@ public class SkinInventory extends CustomInventory {
                         )
                         .editable(false)
                         .onClick(event -> {
+                            Player player = (Player) event.getWhoClicked();
+
+                            player.closeInventory();
+
+                            player.sendMessage(
+                                    language.getMessage("skin.write_the_name_of_skin")
+                            );
+
+                            user.setChangingSkin(true);
                         })
         );
         this.setItem(
@@ -56,6 +78,17 @@ public class SkinInventory extends CustomInventory {
                                 "§fComando: §7/skin atualizar",
                                 "",
                                 "§aClique para atualizar")
+                        .onClick(event -> {
+                            Player player = (Player) event.getWhoClicked();
+
+                            player.closeInventory();
+
+                            SkinManager.change(
+                                    player,
+                                    user,
+                                    player.getName()
+                            );
+                        })
         );
         this.setItem(
                 50,
@@ -68,6 +101,17 @@ public class SkinInventory extends CustomInventory {
                                 "§fComando: §7/skin ajuda",
                                 "",
                                 "§aClique para listar os comandos")
+                        .onClick(event -> {
+                            Player player = (Player) event.getWhoClicked();
+
+                            player.closeInventory();
+
+                            new SkinHelpCommand().onCommand(
+                                    player,
+                                    user,
+                                    null
+                            );
+                        })
         );
 
         this.setCancelled(true);
