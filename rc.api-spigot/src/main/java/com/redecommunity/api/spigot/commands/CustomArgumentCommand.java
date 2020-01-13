8048cee3c27c95b34851927981d641884b0108fc
Gutyerrez <srgutyerrez@gmail.com>
2020-01-13 05:26:26 -0300

    Update CustomArgumentCommand.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/commands/CustomArgumentCommand.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/commands/CustomArgumentCommand.java
@@ -3,6 +3,7 @@ package com.redecommunity.api.spigot.commands;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.command.CommandSender;
 
 /**
  * Created by @SrGutyerrez
@@ -13,5 +14,5 @@ public abstract class CustomArgumentCommand {
     private final Integer index;
     private final String name;
 
-    public abstract void onCommand(User user, String[] args);
+    public abstract void onCommand(CommandSender sender, User user, String[] args);
 }
