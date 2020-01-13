28466cedf528bf535658dc97de2fa90b383656cd
Gutyerrez <srgutyerrez@gmail.com>
2020-01-13 09:45:38 -0300

    Update ChangeSkinAsyncPlayerChatListener.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/listeners/ChangeSkinAsyncPlayerChatListener.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/listeners/ChangeSkinAsyncPlayerChatListener.java
@@ -1,7 +1,6 @@
 package com.redecommunity.api.spigot.skin.listeners;
 
 import com.redecommunity.api.spigot.skin.manager.SkinManager;
-import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 import org.bukkit.entity.Player;
@@ -19,28 +18,9 @@ public class ChangeSkinAsyncPlayerChatListener implements Listener {
 
         User user = UserManager.getUser(player.getUniqueId());
 
-        Language language = user.getLanguage();
-
         if (!user.isChangingSkin()) return;
 
-        if (!user.canChangeSkin()) {
-            player.sendMessage(
-                    String.format(
-                            language.getMessage("skin.wait_to_change_skin"),
-                            user.getTheTimeToTheNextSkinChange()
-                    )
-            );
-            return;
-        }
-
         String skinName = event.getMessage();
-
-        if (!skinName.matches("[a-zA-Z0-9-_]*") || skinName.length() > 16) {
-            player.sendMessage(
-                    language.getMessage("skin.invalid_skin_name")
-            );
-            return;
-        }
 
         SkinManager.change(
                 player,
