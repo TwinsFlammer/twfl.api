a5eadce9e1f77203c0d76ef22c377282c231f1f3
Gutyerrez <srgutyerrez@gmail.com>
2020-01-13 03:06:41 -0300

    Update CustomInventoryManager.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/inventory/manager/CustomInventoryManager.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/inventory/manager/CustomInventoryManager.java
@@ -2,6 +2,7 @@ package com.redecommunity.api.spigot.inventory.manager;
 
 import com.google.common.collect.Lists;
 import com.redecommunity.api.spigot.inventory.CustomInventory;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 import java.util.List;
@@ -17,23 +18,32 @@ public class CustomInventoryManager {
         CustomInventoryManager.inventories.add(customInventory);
     }
 
-    public static CustomInventory getCustomInventory(Inventory inventory) {
+    public static CustomInventory getCustomInventory(Player player) {
         CustomInventoryManager.inventories.forEach(customInventory -> {
             System.out.println(customInventory == null);
             System.out.println(customInventory.getInventory() == null);
 
             System.out.println(customInventory.getInventory().getName());
 
-            System.out.println(inventory.getName());
+            System.out.println(customInventory.getViewers().contains(player));
 
-            System.out.println(customInventory.getHolder().equals(inventory.getHolder()));
+            System.out.println(customInventory.getViewers().stream().anyMatch(humanEntity -> humanEntity.getUniqueId().equals(player.getUniqueId())));
+
+            System.out.println(customInventory.getViewers().stream().anyMatch(humanEntity -> {
+                Player player1 = (Player) humanEntity;
+
+                return player1.getUniqueId().equals(player.getUniqueId());
+            }));
         });
 
         return CustomInventoryManager.inventories
                 .stream()
                 .filter(Objects::nonNull)
-                .filter(customInventory -> customInventory.getInventory() != null)
-                .filter(customInventory -> customInventory.getInventory().equals(inventory))
+                .filter(customInventory ->
+                        customInventory.getViewers()
+                                .stream()
+                                .anyMatch(humanEntity -> humanEntity.getUniqueId().equals(player.getUniqueId()))
+                )
                 .findFirst()
                 .orElse(null);
     }
