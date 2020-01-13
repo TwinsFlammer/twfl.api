dce873f48413baa2744f4082757b5eebe2a080d2
Gutyerrez <srgutyerrez@gmail.com>
2020-01-13 03:13:20 -0300

    Update CustomInventoryManager.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/inventory/manager/CustomInventoryManager.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/inventory/manager/CustomInventoryManager.java
@@ -3,10 +3,8 @@ package com.redecommunity.api.spigot.inventory.manager;
 import com.google.common.collect.Lists;
 import com.redecommunity.api.spigot.inventory.CustomInventory;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by @SrGutyerrez
@@ -19,26 +17,8 @@ public class CustomInventoryManager {
     }
 
     public static CustomInventory getCustomInventory(Player player) {
-        CustomInventoryManager.inventories.forEach(customInventory -> {
-            System.out.println(customInventory == null);
-            System.out.println(customInventory.getInventory() == null);
-
-            System.out.println(customInventory.getInventory().getName());
-
-            System.out.println(customInventory.getViewers().contains(player));
-
-            System.out.println(">>>" + customInventory.getViewers().stream().anyMatch(humanEntity -> humanEntity.getUniqueId().equals(player.getUniqueId())));
-
-            System.out.println(customInventory.getViewers().stream().anyMatch(humanEntity -> {
-                Player player1 = (Player) humanEntity;
-
-                return player1.getUniqueId().equals(player.getUniqueId());
-            }));
-        });
-
         return CustomInventoryManager.inventories
                 .stream()
-                .filter(Objects::nonNull)
                 .filter(customInventory ->
                         customInventory.getViewers()
                                 .stream()
