57a17356be6581dde722d86397fe3f57e5deb31d
Gutyerrez <srgutyerrez@gmail.com>
2020-01-13 05:27:09 -0300

    Update SkinCommand.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/command/SkinCommand.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/command/SkinCommand.java
@@ -1,12 +1,21 @@
 package com.redecommunity.api.spigot.skin.command;
 
+import com.google.common.collect.Maps;
 import com.redecommunity.api.spigot.commands.CustomCommand;
 import com.redecommunity.api.spigot.commands.enums.CommandRestriction;
+import com.redecommunity.api.spigot.skin.command.argument.SkinHelpCommand;
+import com.redecommunity.api.spigot.skin.command.argument.SkinRefreshCommand;
 import com.redecommunity.api.spigot.skin.inventory.SkinInventory;
+import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
-import org.bukkit.Bukkit;
+import com.redecommunity.common.shared.skin.dao.SkinDao;
+import com.redecommunity.common.shared.skin.data.Skin;
+import com.redecommunity.common.shared.skin.factory.SkinFactory;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+
+import java.util.HashMap;
 
 /**
  * Created by @SrGutyerrez
@@ -14,14 +23,59 @@ import org.bukkit.inventory.Inventory;
 public class SkinCommand extends CustomCommand {
     public SkinCommand() {
         super("skin", CommandRestriction.IN_GAME, "default");
+
+        this.addArgument(
+                new SkinRefreshCommand(),
+                new SkinHelpCommand()
+        );
     }
 
     @Override
-    public void onCommand(User user, String[] args) {
+    public void onCommand(CommandSender sender, User user, String[] args) {
+        Language language = user.getLanguage();
+
         Inventory inventory = new SkinInventory(user);
 
-        Player player = Bukkit.getPlayer(user.getUniqueId());
+        Player player = (Player) sender;
 
-        player.openInventory(inventory);
+        if (args.length == 0) {
+            player.openInventory(inventory);
+            return;
+        } else if (args.length == 1) {
+            String skinName = args[0];
+
+            SkinDao skinDao = new SkinDao();
+
+            HashMap<String, String> keys = Maps.newHashMap();
+
+            keys.put("owner", skinName.toLowerCase());
+
+            Skin skin = skinDao.findOne(keys);
+
+            if (skin == null) skin = SkinFactory.getSkin(user.getName());
+
+            if (skin == null) {
+                sender.sendMessage(
+                        language.getMessage("skin.can\'t_find_an_skin_with_this_username")
+                );
+                return;
+            }
+
+            user.setSkin(skin);
+
+            sender.sendMessage(
+                    language.getMessage("skin.changed")
+            );
+            return;
+        } else {
+            sender.sendMessage(
+                    String.format(
+                            language.getMessage("messages.default_commands.invalid_usage"),
+                            this.getName(),
+                            "<nome>"
+                    )
+            );
+            return;
+        }
     }
 }
