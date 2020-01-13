6c77693e027aa15dee58b39bf507c7f3514e8c66
Gutyerrez <srgutyerrez@gmail.com>
2020-01-13 06:08:35 -0300

    Create InvalidBookTitleException.java

---
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/book/exception/InvalidBookTitleException.java
@@ -0,0 +1,10 @@
+package com.redecommunity.api.spigot.book.exception;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class InvalidBookTitleException extends Exception {
+    public InvalidBookTitleException(String message) {
+        super(message);
+    }
+}
