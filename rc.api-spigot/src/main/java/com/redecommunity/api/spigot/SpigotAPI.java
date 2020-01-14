0bf7aa04d0cf34facae2321d9eab1c5a3e94dd0d
Gutyerrez <srgutyerrez@gmail.com>
2020-01-14 07:15:23 -0300

    Update SpigotAPI

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/SpigotAPI.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/SpigotAPI.java
@@ -3,6 +3,8 @@ package com.redecommunity.api.spigot;
 import com.redecommunity.api.shared.API;
 import com.redecommunity.api.spigot.manager.StartManager;
 import com.redecommunity.api.spigot.reflection.Reflection;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 
 /**
  * Created by @SrGutyerrez
@@ -42,5 +44,9 @@ public class SpigotAPI extends CommunityPlugin {
 
     public API getApi() {
         return this.api;
+    }
+
+    public static void unloadUser(User user) {
+        UserManager.getUsers().remove(user);
     }
 }
