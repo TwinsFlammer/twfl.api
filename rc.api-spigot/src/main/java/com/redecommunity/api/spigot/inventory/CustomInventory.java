9f2e107713a6a1b531e6a837c61f3de3b4714643
Gutyerrez <srgutyerrez@gmail.com>
2020-01-15 05:54:47 -0300

    Update CustomInventory.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/inventory/CustomInventory.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/inventory/CustomInventory.java
@@ -144,9 +144,11 @@ public class CustomInventory extends CraftInventory {
             Integer slot = entrySet.getKey();
             Character character = entrySet.getValue();
 
-            CustomItem customItem = this.customItems.get(index).clone();
+            CustomItem customItem = this.customItems.get(index);
 
             if (customItem != null && customItem.isEditable()) {
+                CustomItem customItem1 = customItem.clone();
+
                 super.setItem(index, null);
                 this.customItems.remove(index);
 
