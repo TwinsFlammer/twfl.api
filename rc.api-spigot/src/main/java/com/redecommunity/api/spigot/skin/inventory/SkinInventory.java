beef50262eb6926fffad03839d0cbbe20a602de1
Gutyerrez <srgutyerrez@gmail.com>
2020-01-15 05:33:42 -0300

    Update SinInventory.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/inventory/SkinInventory.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/skin/inventory/SkinInventory.java
@@ -1,5 +1,6 @@
 package com.redecommunity.api.spigot.skin.inventory;
 
+import com.google.common.collect.Maps;
 import com.redecommunity.api.spigot.inventory.CustomPaginateInventory;
 import com.redecommunity.api.spigot.inventory.item.BlockColor;
 import com.redecommunity.api.spigot.inventory.item.CustomItem;
@@ -7,10 +8,14 @@ import com.redecommunity.api.spigot.skin.command.argument.SkinHelpCommand;
 import com.redecommunity.api.spigot.skin.manager.SkinManager;
 import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.common.shared.skin.dao.SkinDao;
+import com.redecommunity.common.shared.skin.data.Skin;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 
 /**
  * Created by @SrGutyerrez
@@ -23,20 +28,60 @@ public class SkinInventory extends CustomPaginateInventory {
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("DD/MM/YYYY");
 
-//        user.getSkins()
-//                .stream()
-//                .sorted((skin1, skin2) -> skin2.getLastUse().compareTo(skin1.getLastUse()))
-//                .forEach(skin -> {
-//                    this.addItem(
-//                            new CustomItem(Material.SKULL_ITEM)
-//                                    .data(3)
-//                                    .name("§e" + skin.getOwner())
-//                                    .lore("§fUsada pela última vez em: §7" + simpleDateFormat.format(skin.getLastUse()),
-//                                            "",
-//                                            (skin.isActive() ? "§aSelecionada." : "§eClique para utilizar essa skin."))
-//                                    .owner(skin.getValue())
-//                    );
-//                });
+        user.getSkins()
+                .stream()
+                .sorted((skin1, skin2) -> skin2.getLastUse().compareTo(skin1.getLastUse()))
+                .forEach(skin -> {
+                    this.addItem(
+                            new CustomItem(Material.SKULL_ITEM)
+                                    .data(3)
+                                    .name("§e" + skin.getOwner())
+                                    .lore("§fUsada pela última vez em: §7" + simpleDateFormat.format(skin.getLastUse()),
+                                            "",
+                                            (skin.isActive() ? "§aSelecionada." : "§eClique para utilizar essa skin."))
+                                    .owner(skin.getValue())
+                                    .onClick(event -> {
+                                        if (skin.isActive()) return;
+
+                                        Player player = (Player) event.getWhoClicked();
+
+                                        SkinDao skinDao = new SkinDao();
+
+                                        HashMap<String, Object> keys = Maps.newHashMap();
+
+                                        Skin active = user.getSkin();
+
+                                        active.setActive(false);
+
+                                        keys.put("active", false);
+
+                                        skinDao.update(
+                                                keys,
+                                                "id",
+                                                active.getId()
+                                        );
+
+                                        skin.setActive(true);
+                                        skin.setLastUse(System.currentTimeMillis());
+
+                                        keys.put("active", skin.isActive());
+                                        keys.put("last_use", skin.getLastUse());
+
+                                        skinDao.update(
+                                                keys,
+                                                "id",
+                                                skin.getId()
+                                        );
+
+                                        User user1 = UserManager.getUser(skin.getOwner());
+
+                                        SkinManager.openBook(
+                                                player,
+                                                user1 == null ? skin.getOwner() : user1.getDisplayName()
+                                        );
+                                    })
+                    );
+                });
 
         this.setItem(
                 48,
