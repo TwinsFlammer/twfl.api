87d3817a61abdc57fea52f77aaead368fea114c7
Gutyerrez <srgutyerrez@gmail.com>
2020-02-07 07:25:32 -0300

    Update AsyncPlayerPreLoginListener.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/listeners/general/AsyncPlayerPreLoginListener.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/listeners/general/AsyncPlayerPreLoginListener.java
@@ -1,5 +1,7 @@
 package com.redecommunity.api.spigot.listeners.general;
 
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
@@ -13,6 +15,10 @@ public class AsyncPlayerPreLoginListener implements Listener {
     @EventHandler
     public void onLogin(AsyncPlayerPreLoginEvent event) {
         UUID uniqueId = event.getUniqueId();
+
+        System.out.println(uniqueId);
+
+//        User user = UserManager.getUser(uniqueId);
 
 
     }
