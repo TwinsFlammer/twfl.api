826a3e61c97879b49e6781c4ea592922802557a2
Gutyerrez <srgutyerrez@gmail.com>
2020-02-07 06:07:18 -0300

    Update SpigotAPI.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/SpigotAPI.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/SpigotAPI.java
@@ -70,10 +70,11 @@ public class SpigotAPI extends CommunityPlugin {
     }
 
     public static Integer getDefaultStatus() {
-
-        System.out.println(SpigotAPI.getInstance().getDataFolder() + File.separator + "configuration.json");
-
         try {
+            File folder = SpigotAPI.getInstance().getDataFolder();
+
+            if (!folder.exists()) folder.mkdirs();
+
             File file = new File(SpigotAPI.getInstance().getDataFolder() + File.separator + "configuration.json");
 
             if (!file.exists()) {
