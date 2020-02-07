6478c38315651395c12188f25b9af375e8f7a4ed
Gutyerrez <srgutyerrez@gmail.com>
2020-02-07 07:31:20 -0300

    Update AsyncPlayerPreLoginListener.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/listeners/general/AsyncPlayerPreLoginListener.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/listeners/general/AsyncPlayerPreLoginListener.java
@@ -1,11 +1,16 @@
 package com.redecommunity.api.spigot.listeners.general;
 
+import com.redecommunity.api.spigot.SpigotAPI;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.group.dao.UserGroupDao;
+import com.redecommunity.common.shared.permissions.user.group.data.UserGroup;
 import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.common.shared.server.data.Server;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -16,10 +21,16 @@ public class AsyncPlayerPreLoginListener implements Listener {
     public void onLogin(AsyncPlayerPreLoginEvent event) {
         UUID uniqueId = event.getUniqueId();
 
-        System.out.println(uniqueId);
+        User user = UserManager.getUser(uniqueId);
 
-//        User user = UserManager.getUser(uniqueId);
+        UserGroupDao userGroupDao = new UserGroupDao();
 
+        Server server = SpigotAPI.getCurrentServer();
 
+        String serverId = "00 || " + server.getId();
+
+        Set<UserGroup> groups = userGroupDao.findAll(user.getId(), serverId);
+
+        user.setGroups(groups);
     }
 }
