e982d7a564fe1bd1b4662b6ffa2aaffe90d32620
Gutyerrez <srgutyerrez@gmail.com>
2020-02-16 14:09:05 -0300

    Update pom.xml

--- rf.api-bungee/src/main/java/com/redefocus/api/bungeecord/commands/defaults/disable/EnableCommand.java
+++ rf.api-bungee/src/main/java/com/redefocus/api/bungeecord/commands/defaults/disable/EnableCommand.java
@@ -1,12 +1,12 @@
-package com.redecommunity.api.bungeecord.commands.defaults.disable;
+package com.redefocus.api.bungeecord.commands.defaults.disable;
 
-import com.redecommunity.api.bungeecord.commands.CustomCommand;
-import com.redecommunity.api.bungeecord.commands.enums.CommandRestriction;
-import com.redecommunity.api.shared.commands.defaults.disable.dao.DisabledCommandDao;
-import com.redecommunity.api.shared.commands.defaults.disable.data.DisabledCommand;
-import com.redecommunity.api.shared.commands.defaults.disable.manager.DisabledCommandManager;
-import com.redecommunity.common.shared.language.enums.Language;
-import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redefocus.api.bungeecord.commands.CustomCommand;
+import com.redefocus.api.bungeecord.commands.enums.CommandRestriction;
+import com.redefocus.api.shared.commands.defaults.disable.dao.DisabledCommandDao;
+import com.redefocus.api.shared.commands.defaults.disable.data.DisabledCommand;
+import com.redefocus.api.shared.commands.defaults.disable.manager.DisabledCommandManager;
+import com.redefocus.common.shared.language.enums.Language;
+import com.redefocus.common.shared.permissions.user.data.User;
 
 /**
  * Created by @SrGutyerrez
