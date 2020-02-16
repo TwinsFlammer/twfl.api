2410d4aa64b91a338693bbba9a43ca3205b58dfd
Gutyerrez <srgutyerrez@gmail.com>
2020-02-16 14:14:37 -0300

    Rename package from redecommunity to redefocus

--- rf.api-bungee/src/main/java/com/redefocus/api/bungeecord/FocusPlugin.java
+++ rf.api-bungee/src/main/java/com/redefocus/api/bungeecord/FocusPlugin.java
@@ -1,7 +1,7 @@
-package com.redecommunity.api.bungeecord;
+package com.redefocus.api.bungeecord;
 
-import com.redecommunity.common.shared.Common;
-import com.redecommunity.common.shared.updater.data.Updater;
+import com.redefocus.common.shared.Common;
+import com.redefocus.common.shared.updater.data.Updater;
 import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.api.plugin.Plugin;
 
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Created by @SrGutyerrez
  */
 @RequiredArgsConstructor
-public abstract class CommunityPlugin extends Plugin {
+public abstract class FocusPlugin extends Plugin {
     @Override
     public void onLoad() {
     }
