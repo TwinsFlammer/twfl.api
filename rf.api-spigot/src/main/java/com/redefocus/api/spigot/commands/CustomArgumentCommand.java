27959cf75ac85122d53b4fa140d3e8e1d36c63a4
Gutyerrez <srgutyerrez@gmail.com>
2020-02-16 14:21:26 -0300

    Rename package from redecommunity to redefocus

--- rf.api-spigot/src/main/java/com/redefocus/api/spigot/commands/CustomArgumentCommand.java
+++ rf.api-spigot/src/main/java/com/redefocus/api/spigot/commands/CustomArgumentCommand.java
@@ -1,6 +1,6 @@
-package com.redecommunity.api.spigot.commands;
+package com.redefocus.api.spigot.commands;
 
-import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.permissions.user.data.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.command.CommandSender;
