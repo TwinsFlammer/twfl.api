b90e68a7fd7703f95479fc3cd7acd9c4d90c19b8
Gutyerrez <srgutyerrez@gmail.com>
2020-02-29 18:38:40 -0300

    Update CommunityPlugin.java

--- rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/CommunityPlugin.java
+++ rc.api-bungee/src/main/java/com/redecommunity/api/bungeecord/CommunityPlugin.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Created by @SrGutyerrez
  */
 @RequiredArgsConstructor
-public abstract class FocusPlugin extends Plugin {
+public abstract class CommunityPlugin extends Plugin {
     @Override
     public void onLoad() {
     }
