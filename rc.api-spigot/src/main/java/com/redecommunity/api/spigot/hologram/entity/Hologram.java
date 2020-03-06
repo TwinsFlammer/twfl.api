38079092bd6578cb14e5c3ac52e5ea8409e17c54
Gutyerrez <srgutyerrez@gmail.com>
2020-03-06 06:47:06 -0300

    Update Hologram.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/hologram/entity/Hologram.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/hologram/entity/Hologram.java
@@ -1,12 +1,9 @@
 package com.redecommunity.api.spigot.hologram.entity;
 
 import com.redecommunity.api.spigot.hologram.util.NullBoundingBox;
-import net.minecraft.server.v1_8_R3.World;
 import net.minecraft.server.v1_8_R3.EntityArmorStand;
+import net.minecraft.server.v1_8_R3.World;
 
-/**
- * @author Enzo
- */
 public class Hologram extends EntityArmorStand {
 
     public Hologram(World world) {
@@ -14,5 +11,6 @@ public class Hologram extends EntityArmorStand {
 
         this.a(new NullBoundingBox());
         this.setSize(0.0f, 0.0f);
+        this.setGravity(false);
     }
 }
