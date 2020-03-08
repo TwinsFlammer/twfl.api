49b2d4f259e81bc18c8954845b185d3620d82a4a
Gutyerrez <srgutyerrez@gmail.com>
2020-03-08 20:50:58 -0300

    Update PreferenceStateChangeEvent.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/preference/event/PreferenceStateChangeEvent.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/preference/event/PreferenceStateChangeEvent.java
@@ -3,7 +3,10 @@ package com.redecommunity.api.spigot.preference.event;
 import com.redecommunity.api.spigot.event.CommunityEvent;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.preference.Preference;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 
 /**
@@ -11,9 +14,16 @@ import org.bukkit.event.Cancellable;
  */
 @RequiredArgsConstructor
 public class PreferenceStateChangeEvent extends CommunityEvent implements Cancellable {
+    @Getter
     private final User user;
+    @Getter
     private final Preference preference;
+
     private Boolean cancelled = false;
+
+    public Player getPlayer() {
+        return Bukkit.getPlayer(this.user.getUniqueId());
+    }
 
     @Override
     public boolean isCancelled() {
