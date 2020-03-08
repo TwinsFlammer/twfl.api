4eb8b32344fca9c4d5c21469782dc87ce3d1d48a
Gutyerrez <srgutyerrez@gmail.com>
2020-03-08 18:54:15 -0300

    Create PreferenceStateChangeEvent.java

---
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/preference/event/PreferenceStateChangeEvent.java
@@ -0,0 +1,27 @@
+package com.redecommunity.api.spigot.preference.event;
+
+import com.redecommunity.api.spigot.event.CommunityEvent;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.preference.Preference;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.event.Cancellable;
+
+/**
+ * Created by @SrGutyerrez
+ */
+@RequiredArgsConstructor
+public class PreferenceStateChangeEvent extends CommunityEvent implements Cancellable {
+    private final User user;
+    private final Preference preference;
+    private Boolean cancelled;
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
+    }
+}
