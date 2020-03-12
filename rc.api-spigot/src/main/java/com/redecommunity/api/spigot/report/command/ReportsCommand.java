9840808788f448df6a05604de7416cc7969a12e8
Gutyerrez <srgutyerrez@gmail.com>
2020-03-12 05:05:26 -0300

    Update ReportsCommand.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/report/command/ReportsCommand.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/report/command/ReportsCommand.java
@@ -2,9 +2,11 @@ package com.redecommunity.api.spigot.report.command;
 
 import com.redecommunity.api.spigot.commands.CustomCommand;
 import com.redecommunity.api.spigot.commands.enums.CommandRestriction;
+import com.redecommunity.api.spigot.report.inventory.ReportsInventory;
 import com.redecommunity.common.shared.permissions.group.GroupNames;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  * Created by @SrGutyerrez
@@ -16,6 +18,11 @@ public class ReportsCommand extends CustomCommand {
 
     @Override
     public void onCommand(CommandSender sender, User user, String[] args) {
+        Player player = (Player) sender;
 
+        player.openInventory(
+                new ReportsInventory()
+                        .build()
+        );
     }
 }
