52dc1e759c6851db11956c8381931db45dda0f42
Gutyerrez <srgutyerrez@gmail.com>
2020-03-12 18:30:20 -0300

    Update TeleportRequestRunnable.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/teleport/runnable/TeleportRequestRunnable.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/teleport/runnable/TeleportRequestRunnable.java
@@ -1,6 +1,5 @@
 package com.redecommunity.api.spigot.teleport.runnable;
 
-import com.redecommunity.api.spigot.teleport.data.TeleportRequest;
 import com.redecommunity.api.spigot.teleport.exception.InvalidTeleportTargetServerException;
 import com.redecommunity.api.spigot.teleport.manager.TeleportRequestManager;
 
@@ -11,14 +10,18 @@ public class TeleportRequestRunnable implements Runnable {
     @Override
     public void run() {
         TeleportRequestManager.getTeleportRequests()
-                .stream()
-                .filter(TeleportRequest::canTeleport)
-                .forEach(teleportRequest -> {
-                    try {
-                        teleportRequest.teleport();
-                    } catch (InvalidTeleportTargetServerException exception) {
-                        exception.printStackTrace();
+                .removeIf(teleportRequest -> {
+                    Boolean canTeleport = teleportRequest.canTeleport();
+
+                    if (canTeleport) {
+                        try {
+                            teleportRequest.teleport();
+                        } catch (InvalidTeleportTargetServerException exception) {
+                            exception.printStackTrace();
+                        }
                     }
+
+                    return canTeleport;
                 });
     }
 }
