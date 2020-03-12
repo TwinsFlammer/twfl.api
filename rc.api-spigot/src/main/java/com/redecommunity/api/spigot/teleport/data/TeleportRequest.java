8ee88b569fe2c4865b63b41831a73c5008454345
Gutyerrez <srgutyerrez@gmail.com>
2020-03-12 17:27:18 -0300

    Update TeleportRequest.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/teleport/data/TeleportRequest.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/teleport/data/TeleportRequest.java
@@ -1,13 +1,19 @@
 package com.redecommunity.api.spigot.teleport.data;
 
+import com.redecommunity.api.spigot.SpigotAPI;
 import com.redecommunity.api.spigot.teleport.channel.TeleportChannel;
 import com.redecommunity.api.spigot.teleport.exception.InvalidTeleportTargetServerException;
+import com.redecommunity.api.spigot.teleport.manager.TeleportRequestManager;
 import com.redecommunity.api.spigot.util.serialize.LocationSerialize;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 import com.redecommunity.common.shared.server.data.Server;
 import com.redecommunity.common.shared.server.manager.ServerManager;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.json.simple.JSONObject;
 
 /**
@@ -21,11 +27,32 @@ public class TeleportRequest {
     private final Integer targetServerId;
     private final Long teleportTime;
 
+    public void start() {
+        TeleportRequestManager.getTeleportRequests().add(this);
+    }
+
     public void teleport() throws InvalidTeleportTargetServerException {
         Server server = ServerManager.getServer(this.targetServerId);
 
         if (server == null)
             throw new InvalidTeleportTargetServerException("Can\'t find the target server id: " + this.targetServerId);
+
+        if (server.getId().equals(SpigotAPI.getCurrentServer().getId())) {
+            User user = UserManager.getUser(this.userId);
+
+            Player player = Bukkit.getPlayer(user.getUniqueId());
+
+            if (this.targetId == null) {
+                player.teleport(this.targetLocation);
+            } else {
+                User user1 = UserManager.getUser(this.targetId);
+
+                Player player1 = Bukkit.getPlayer(user1.getUniqueId());
+
+                player.teleport(player1);
+            }
+            return;
+        }
 
         String serializedLocation = targetLocation == null ? null : LocationSerialize.toString(this.targetLocation);
 
