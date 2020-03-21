f0223c98324af2902b8695566cf97947046dd964
Gutyerrez <srgutyerrez@gmail.com>
2020-03-21 07:55:50 -0300

    Update CustomArgumentCommand.java

--- rc.api-spigot/src/main/java/com/redecommunity/api/spigot/commands/CustomArgumentCommand.java
+++ rc.api-spigot/src/main/java/com/redecommunity/api/spigot/commands/CustomArgumentCommand.java
@@ -2,17 +2,55 @@ package com.redecommunity.api.spigot.commands;
 
 import com.redecommunity.common.shared.permissions.user.data.User;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.bukkit.command.CommandSender;
 
 /**
  * Created by @SrGutyerrez
  */
-@RequiredArgsConstructor
-@Getter
-public abstract class CustomArgumentCommand {
+public abstract class CustomArgumentCommand<C extends CustomCommand> {
+    @Getter
     private final Integer index;
-    private final String name;
+    @Getter
+    private final String name, description;
+
+    private C command;
+
+    public CustomArgumentCommand(Integer index, String name) {
+        this.index = index;
+        this.name = name;
+        this.description = "";
+    }
+
+    public CustomArgumentCommand(Integer index, String name, String description) {
+        this.index = index;
+        this.name = name;
+        this.description = description;
+    }
+
+    public CustomArgumentCommand(Integer index, String name, C command) {
+        this.index = index;
+        this.name = name;
+        this.description = "";
+
+        this.command = command;
+    }
+
+    public CustomArgumentCommand(Integer index, String name, String description, C command) {
+        this.index = index;
+        this.name = name;
+        this.description = description;
+
+        this.command = command;
+    }
+
 
     public abstract void onCommand(CommandSender sender, User user, String[] args);
+
+    public String getUsage() {
+        return this.command == null ? "" : String.format("§cUtilize /%s %s.", this.command.getName(), this.name);
+    }
+
+    public String getUsage(String arguments) {
+        return this.command == null ? "" : String.format("§cUtilize /%s %s <%s>.", this.command.getName(), this.name, arguments);
+    }
 }
