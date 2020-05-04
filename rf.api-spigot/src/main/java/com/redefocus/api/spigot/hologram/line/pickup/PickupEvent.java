22b458403f863090965573d9acd864567767ff27
Gutyerrez <srgutyerrez@gmail.com>
2020-05-04 16:15:03 -0300

    Rename from "rc" to "rf"

---
+++ rf.api-spigot/src/main/java/com/redefocus/api/spigot/hologram/line/pickup/PickupEvent.java
@@ -0,0 +1,9 @@
+package com.redefocus.api.spigot.hologram.line.pickup;
+
+import org.bukkit.entity.Player;
+
+public interface PickupEvent {
+
+    void onPickup(Player player);
+
+}
---
+++ rf.api-spigot/src/main/java/com/redefocus/api/spigot/util/jsontext/listener/JSONTextJedisMessageListener.java
@@ -0,0 +1,37 @@
+package com.redefocus.api.spigot.util.jsontext.listener;
+
+import com.redefocus.api.spigot.SpigotAPI;
+import com.redefocus.api.spigot.user.data.SpigotUser;
+import com.redefocus.api.spigot.util.jsontext.channel.JSONTextChannel;
+import com.redefocus.api.spigot.util.jsontext.data.JSONText;
+import com.redefocus.common.shared.databases.redis.handler.JedisMessageListener;
+import com.redefocus.common.shared.databases.redis.handler.annonation.ChannelName;
+import com.redefocus.common.shared.databases.redis.handler.event.JedisMessageEvent;
+import org.bukkit.entity.Player;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+/**
+ * @author SrGutyerrez
+ */
+public class JSONTextJedisMessageListener implements JedisMessageListener {
+    @ChannelName(name = JSONTextChannel.CHANNEL_NAME)
+    public void onMessage(JedisMessageEvent event) {
+        String message = event.getMessage();
+
+        JSONObject jsonObject = (JSONObject) JSONValue.parse(message);
+
+        Integer userId = ((Long) jsonObject.get("user_id")).intValue();
+        String text = (String) jsonObject.get("json_text");
+
+        SpigotUser spigotUser = SpigotAPI.getSpigotUserFactory().getUser(userId);
+
+        Player player = spigotUser.getPlayer();
+
+        if (player != null) {
+            JSONText jsonText = JSONText.fromString(text);
+
+            jsonText.send(player);
+        }
+    }
+}
