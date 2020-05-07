fc0752228020359b9331530bbbefa4259b18521f
Gutyerrez <srgutyerrez@gmail.com>
2020-05-07 15:57:03 -0300

    Update CommunityEvent.java

--- rf.api-spigot/src/main/java/com/redefocus/api/spigot/event/CommunityEvent.java
+++ rf.api-spigot/src/main/java/com/redefocus/api/spigot/event/CommunityEvent.java
@@ -1,25 +1,8 @@
 package com.redefocus.api.spigot.event;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
-
 /**
- * Created by @SrGutyerrez
+ * @author SrGutyerrez
  */
-public class CommunityEvent extends Event {
-    public static HandlerList handler = new HandlerList();
-
-    public static HandlerList getHandlerList() {
-        return CommunityEvent.handler;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return CommunityEvent.handler;
-    }
-
-    public void run() {
-        Bukkit.getPluginManager().callEvent(this);
-    }
+@Deprecated
+public class CommunityEvent extends FocusEvent {
 }
