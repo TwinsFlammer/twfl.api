869e90fcd397fc87d3d818f773b3c83265b3286d
Gutyerrez <srgutyerrez@gmail.com>
2020-05-08 20:28:52 -0300

    Update TeleportRequestRunnable.java

--- rf.api-spigot/src/main/java/com/redefocus/api/spigot/teleport/runnable/TeleportRequestRunnable.java
+++ rf.api-spigot/src/main/java/com/redefocus/api/spigot/teleport/runnable/TeleportRequestRunnable.java
@@ -1,7 +1,11 @@
 package com.redefocus.api.spigot.teleport.runnable;
 
+import com.redefocus.api.spigot.SpigotAPI;
 import com.redefocus.api.spigot.teleport.exception.InvalidTeleportTargetServerException;
 import com.redefocus.api.spigot.teleport.manager.TeleportRequestManager;
+import com.redefocus.api.spigot.user.data.SpigotUser;
+import com.redefocus.api.spigot.util.title.data.CustomTitle;
+import com.redefocus.common.shared.util.TimeFormatter;
 
 /**
  * Created by @SrGutyerrez
@@ -11,17 +15,34 @@ public class TeleportRequestRunnable implements Runnable {
     public void run() {
         TeleportRequestManager.getTeleportRequests()
                 .removeIf(teleportRequest -> {
-                    Boolean canTeleport = teleportRequest.canTeleport();
+                    SpigotUser spigotUser = SpigotAPI.getSpigotUserFactory().getUser(teleportRequest.getUserId());
 
-                    if (canTeleport) {
-                        try {
-                            teleportRequest.teleport();
-                        } catch (InvalidTeleportTargetServerException exception) {
-                            exception.printStackTrace();
+                    CustomTitle.sendTitle(
+                            spigotUser.getPlayer(),
+                            0,
+                            30,
+                            0,
+                            "§eTeletransportando",
+                            "§eEm " + TimeFormatter.formatMinimized(
+                                    teleportRequest.getRemainingTime()
+                            ) + "..."
+                    );
+
+                    try {
+                        if (teleportRequest.canTeleport()) {
+                            try {
+                                teleportRequest.teleport();
+                            } catch (InvalidTeleportTargetServerException exception) {
+                                exception.printStackTrace();
+                            }
+
+                            return true;
                         }
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
                     }
 
-                    return canTeleport;
+                    return false;
                 });
     }
 }
