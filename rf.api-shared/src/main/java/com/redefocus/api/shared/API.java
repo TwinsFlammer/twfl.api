88812deb25362c601b3ae3222550f44e3ea67b88
Gutyerrez <srgutyerrez@gmail.com>
2020-05-09 02:57:44 -0300

    Update API.java

--- rf.api-shared/src/main/java/com/redefocus/api/shared/API.java
+++ rf.api-shared/src/main/java/com/redefocus/api/shared/API.java
@@ -1,12 +1,26 @@
 package com.redefocus.api.shared;
 
+import com.redefocus.api.shared.cash.event.ICashChangeEvent;
 import com.redefocus.api.shared.manager.StartManager;
+import lombok.Getter;
 
 /**
  * Created by @SrGutyerrez
  */
 public class API {
+    @Getter
+    private static API instance;
+
+    @Getter
+    private ICashChangeEvent ICashChangeEvent;
+
     public API() {
+        API.instance = this;
+
         new StartManager();
+    }
+
+    public void setICashChangeEvent(ICashChangeEvent ICashChangeEvent) {
+        this.ICashChangeEvent = ICashChangeEvent;
     }
 }
