6fd9948f2241ec5fc90d77ac1d83574906c678a6
Gutyerrez <srgutyerrez@gmail.com>
2020-05-09 02:58:09 -0300

    Create CashChannel.java

---
+++ rf.api-shared/src/main/java/com/redefocus/api/shared/cash/channel/CashChannel.java
@@ -0,0 +1,27 @@
+package com.redefocus.api.shared.cash.channel;
+
+import com.redefocus.common.shared.databases.redis.channel.data.Channel;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisDataException;
+
+/**
+ * @author SrGutyerrez
+ */
+public class CashChannel extends Channel {
+    @Override
+    public String getName() {
+        return "cash_channel";
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        try (Jedis jedis = this.getJedisPool().getResource()) {
+            jedis.publish(
+                    this.getName(),
+                    message
+            );
+        } catch (JedisDataException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
