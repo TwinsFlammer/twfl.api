dc9bde7516d69bc350e375df378651aff7079fc1
Gutyerrez <srgutyerrez@gmail.com>
2020-05-09 03:07:25 -0300

    Update ICashChangeEvent.java

--- rf.api-shared/src/main/java/com/redefocus/api/shared/cash/event/ICashChangeEvent.java
+++ rf.api-shared/src/main/java/com/redefocus/api/shared/cash/event/ICashChangeEvent.java
@@ -1,8 +1,10 @@
 package com.redefocus.api.shared.cash.event;
 
+import com.redefocus.common.shared.permissions.user.data.User;
+
 /**
  * @author SrGutyerrez
  */
 public interface ICashChangeEvent {
-    void dispatchCashChange(Integer newAmount, Integer oldAmount);
+    void dispatchCashChange(User user, Integer newAmount, Integer oldAmount);
 }
