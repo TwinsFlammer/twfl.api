7f707e79834d012832e941c7cbdbe08a301f2405
Gutyerrez <srgutyerrez@gmail.com>
2020-05-21 01:20:34 -0300

    Update CommandRegistry.java

--- twfl.api-bungee/src/main/java/br/com/twinsflammer/api/bungeecord/commands/registry/CommandRegistry.java
+++ twfl.api-bungee/src/main/java/br/com/twinsflammer/api/bungeecord/commands/registry/CommandRegistry.java
@@ -1,6 +1,6 @@
 package br.com.twinsflammer.api.bungeecord.commands.registry;
 
-import br.com.twinsflammer.api.bungeecord.FocusPlugin;
+import br.com.twinsflammer.api.bungeecord.TwinsPlugin;
 import br.com.twinsflammer.api.bungeecord.commands.CustomCommand;
 import net.md_5.bungee.api.ProxyServer;
 
@@ -8,9 +8,9 @@ import net.md_5.bungee.api.ProxyServer;
  * Created by @SrGutyerrez
  */
 public class CommandRegistry {
-    public static void registerCommand(FocusPlugin focusPlugin, CustomCommand customCommand) {
-        System.out.println("[CommandRegistry] Registering " + customCommand.getName() + " command by " + focusPlugin.getFile().getName() + ".");
+    public static void registerCommand(TwinsPlugin twinsPlugin, CustomCommand customCommand) {
+        System.out.println("[CommandRegistry] Registering " + customCommand.getName() + " command by " + twinsPlugin.getFile().getName() + ".");
 
-        ProxyServer.getInstance().getPluginManager().registerCommand(focusPlugin, customCommand);
+        ProxyServer.getInstance().getPluginManager().registerCommand(twinsPlugin, customCommand);
     }
 }
